f6bcf832d7d1dcc970330d7c2dcc73ac7fe7907d
Eduardo <dj.shadowjeab@gmail.com>
2024-07-22 22:31:06 -0500

    ev1

--- 02 JAVASCRIPT/capitulo 02/js/codigo.js
+++ 02 JAVASCRIPT/capitulo 02/js/codigo.js
@@ -1,1 +1,15 @@
-console.log('funciona');
+let tareaInput = document.querySelector('.tarea');
+
+let btn = document.querySelector('button');
+
+let tareasUl = document.querySelector('ul');
+// btn.addEventListener('click', function () {
+//     console.log(this);
+// }) 
+
+btn.addEventListener('click', () => {
+    console.log('hiciste clik');
+    console.log(tareaInput.value);
+    tareasUl.innerHTML = `<li>${tareaInput.value}</li>`;
+});
+
