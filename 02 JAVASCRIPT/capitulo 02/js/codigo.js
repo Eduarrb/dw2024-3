0e1d9bf28c686634f1c42c48efcbb1acf6244da1
Eduardo <dj.shadowjeab@gmail.com>
2024-07-24 20:45:19 -0500

    popup

--- 02 JAVASCRIPT/capitulo 02/js/codigo.js
+++ 02 JAVASCRIPT/capitulo 02/js/codigo.js
@@ -1,15 +1,48 @@
-let tareaInput = document.querySelector('.tarea');
+const tareaInput = document.querySelector('.tarea');
+const btn = document.querySelector('button');
+const tareasUl = document.querySelector('ul');
+const alerta = document.querySelector('.alerta');
 
-let btn = document.querySelector('button');
+// console.log(tareasInsertadas);
 
-let tareasUl = document.querySelector('ul');
 // btn.addEventListener('click', function () {
 //     console.log(this);
 // }) 
 
 btn.addEventListener('click', () => {
-    console.log('hiciste clik');
-    console.log(tareaInput.value);
-    tareasUl.innerHTML = `<li>${tareaInput.value}</li>`;
+    // console.log('hiciste clik');
+    // console.log(tareaInput.value);
+    // tareasUl.innerHTML = `<li>${tareaInput.value}</li>`;
+    // 🌟🌟 2 tipos de igualdad, la simple y la estricta
+    if (tareaInput.value === ''){
+        alerta.textContent = 'No debes ingresar una tarea en blanco';
+    } else {
+        alerta.textContent = '';
+        const item = `<li>${tareaInput.value}</li>`;
+        tareasUl.insertAdjacentHTML('beforeend', item);
+        tareaInput.value = '';
+    }
 });
 
+// 💥💥 LA MANERA INCORRECTA
+
+// const tareasInsertadas = document.querySelectorAll('li');
+
+// for (let i = 0; i < tareasInsertadas.length; i++){
+//     // console.log(tareasInsertadas[i]);
+//     tareasInsertadas[i].addEventListener('click', function(){
+//         // console.log('hiciste click');
+//         tareasInsertadas[i].remove();
+//     })
+// }
+
+
+// ⚡⚡ DELEGACION DE EVENTOS
+
+tareasUl.addEventListener('click', function(e){
+    // console.log('hiciste click');
+    // console.log(e.target.tagName);
+    if(e.target.tagName === 'LI'){
+        e.target.remove();
+    }
+});
