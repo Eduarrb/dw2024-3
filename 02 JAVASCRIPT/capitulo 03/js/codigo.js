6b558cca924ce8a05832ad3e17243dc9cdb2adec
Eduardo <dj.shadowjeab@gmail.com>
2024-07-24 22:30:57 -0500

    cap4

---
+++ 02 JAVASCRIPT/capitulo 03/js/codigo.js
@@ -0,0 +1,25 @@
+const btn = document.querySelector('button');
+const popupCaja = document.querySelector('.popup-caja');
+const btnClose = document.querySelector('.popup-close');
+
+btn.addEventListener('click', () => {
+    popupCaja.classList.add('mostrarCaja');
+});
+
+btnClose.addEventListener('click', () => {
+    popupCaja.classList.remove('mostrarCaja');
+});
+
+popupCaja.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.classList.contains('popup-caja')){
+        popupCaja.classList.remove('mostrarCaja');
+    }
+});
+
+window.addEventListener('keyup', e => {
+    // console.log(e.code);
+    if(e.code === "Escape"){
+        popupCaja.classList.remove('mostrarCaja');
+    }
+});
