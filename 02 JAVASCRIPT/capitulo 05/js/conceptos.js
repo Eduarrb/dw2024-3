57bcbe74f0bbd14c9f47dab0d1e8063213fbff33
Eduardo <dj.shadowjeab@gmail.com>
2024-07-26 21:15:33 -0500

    async

---
+++ 02 JAVASCRIPT/capitulo 05/js/conceptos.js
@@ -0,0 +1,13 @@
+
+// ⚡⚡ EJECUCIONES ASINCORNAS ⚡⚡
+
+setInterval(function(){
+    console.log('hiciste una peticion');
+}, 3000)
+
+// 
+// ⚡⚡ EJECUCIONES SINCRONAS ⚡⚡
+
+const nombre = 'Jhonel';
+
+console.log(nombre);
