809d7f1f556bd3b7defbf554dd114460524539aa
Eduardo <dj.shadowjeab@gmail.com>
2024-08-07 20:56:23 -0500

    rel

--- 03 MYSQL Y PHP/01 MYSQL/02 relaciones.sql
+++ 03 MYSQL Y PHP/01 MYSQL/02 relaciones.sql
@@ -18,3 +18,55 @@ INSERT INTO personajes (per_act_id, per_peli_id, per_nombre) VALUES
 
 -- ⚡⚡ JOINS ⚡⚡
 SELECT * FROM personajes INNER JOIN peliculas ON personajes.per_peli_id = peliculas.peli_id
+
+-- ⚡⚡ ALIAS PARA TABLAS
+
+SELECT * FROM personajes a INNER JOIN peliculas b ON a.per_peli_id = b.peli_id
+
+SELECT * FROM personajes a INNER JOIN actores b ON a.per_act_id = b.act_id
+
+CREATE TABLE directores (
+    dire_id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    dire_nombres VARCHAR(100) NOT NULL,
+    dire_apellidos VARCHAR(100) NOT NULL
+)
+
+INSERT INTO directores (dire_nombres, dire_apellidos) VALUES
+    ('Christopher', 'Nolan'),
+    ('Lana', 'Wachowsky'),
+    ('James', 'Cameron'),
+    ('Ridley', 'Scott'),
+    ('Juan', 'Bayona')
+
+INSERT INTO directores (dire_nombres, dire_apellidos) VALUES
+    ('Peter', 'Jackson')
+
+ALTER TABLE peliculas ADD COLUMN peli_dire_id INT AFTER peli_id
+
+-- ⚡⚡ UPDATE
+UPDATE peliculas SET peli_dire_id = 1 WHERE peli_id = 1
+UPDATE peliculas SET peli_dire_id = 2 WHERE peli_id = 2
+UPDATE peliculas SET peli_dire_id = 3 WHERE peli_id = 3
+UPDATE peliculas SET peli_dire_id = 4 WHERE peli_id = 7
+UPDATE peliculas SET peli_dire_id = 5 WHERE peli_id = 5
+
+SELECT * FROM peliculas a INNER JOIN directores b ON a.peli_dire_id = b.dire_id
+
+-- LEFT
+SELECT * FROM peliculas a LEFT JOIN directores b ON a.peli_dire_id = b.dire_id
+
+SELECT * FROM peliculas pel RIGHT JOIN directores dir ON pel.peli_dire_id = dir.dire_id
+
+---------------------------------------------------------------------------------------
+-- ⚡⚡ 3 TABLAS
+SELECT *
+    FROM directores a
+        INNER JOIN peliculas b ON a.dire_id = b.peli_dire_id
+        INNER JOIN personajes c ON b.peli_id = c.per_peli_id
+
+    
+SELECT *
+    FROM directores a
+        INNER JOIN peliculas b ON a.dire_id = b.peli_dire_id
+        INNER JOIN personajes c ON b.peli_id = c.per_peli_id
+        RIGHT JOIN actores d ON d.act_id = c.per_act_id
