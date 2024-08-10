12d6fb926f0d159045472bd29b55c9d2ce619490
Eduardo <dj.shadowjeab@gmail.com>
2024-08-09 20:06:29 -0500

    rel

---
+++ 03 MYSQL Y PHP/01 MYSQL/03 llaves.sql
@@ -0,0 +1,23 @@
+-- ⚡⚡ LLAVES PRIMARIAS Y LLAVES FORANEAS ⚡⚡
+-- OJO: AL MOMENTO DE RELACIONAR TABLAS, LOS CAMPOS EN COMUN DEBEN SER DEL MISMO DE DATO Y CARACTERISTICAS.
+
+-- RESTRICCIONES
+-- RESTRICT:
+    -- Por defecto, impide realizar modificaciones que atenten la seguridad referencial de la tablas.
+    -- no permite borrar o actualizar datos
+
+-- CASCADE:
+    -- Al actualizar o borrar los datos de referencia, también se actualiza o se borra el dato de la tabla dependiente
+
+-- SET NULL:
+    -- Se establece campos nulos a la ytabla dependiente
+
+-- NO ACTION:
+    -- No hace nada.
+
+ALTER TABLE peliculas CHANGE COLUMN peli_dire_id peli_dire_id INT UNSIGNED
+
+ALTER TABLE peliculas
+    ADD CONSTRAINT fk_direId FOREIGN KEY (peli_dire_id)
+    REFERENCES directores (dire_id)
+    ON DELETE RESTRICT ON UPDATE RESTRICT
