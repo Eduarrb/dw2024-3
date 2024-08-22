aa6642d46b1e33a1025f714f39632e46134f1d16
Eduardo <dj.shadowjeab@gmail.com>
2024-08-21 22:39:24 -0500

    con

--- 04 CMS/resources/controllers/emailController.php
+++ 04 CMS/resources/controllers/emailController.php
@@ -10,8 +10,8 @@
         $mail->isSmtp();
         $mail->Host = 'sandbox.smtp.mailtrap.io';
         $mail->SMTPAuth = true;
-        $mail->Username = '6bcb7fc6b0c726';
-        $mail->Password = '62fe91ab669d57';
+        $mail->Username = '';
+        $mail->Password = '';
         $mail->Port = 25;
         $mail->SMTPSecure = 'tls';
         $mail->isHTML(true);
