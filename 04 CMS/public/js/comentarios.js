7645dd194638cb1a0acba5427e6e9a08396492ad
Eduardo <dj.shadowjeab@gmail.com>
2024-09-09 20:58:44 -0500

    com

--- 04 CMS/public/js/comentarios.js
+++ 04 CMS/public/js/comentarios.js
@@ -1,18 +1,54 @@
-// axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
-//     .then(res => {
-//         console.log(res.data);
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
+const cajaComentarios = document.querySelector('.comentarios__container__box');
+const url = location.href;
+const urlArray = url.split("=");
+const id = +urlArray[1];
+
+const fechaOptions = {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric'
+}
+
+const renderComentarios = objComentario => {
+    const fecha = new Date(objComentario.com_fecha);
+
+    return `
+        <div class="comentarios__container__box__item">
+            <div class="comentarios__container__box__item__imgBox">
+                <img src="img/${!objComentario.user_img ? 'user.png' : objComentario.user_img}" alt="">
+            </div>
+            <div class="comentarios__container__box__item__data">
+                <div class="comentarios__container__box__item__data__top">
+                    <span>${objComentario.usuario}</span>
+                    <span>${fecha.toLocaleDateString("es-ES", fechaOptions)}</span>
+                </div>
+                <p class="comentarios__container__box__item__data__descri mt-1">
+                    ${objComentario.com_mensaje}
+                </p>
+                <div class="comentarios__container__box__item__data__stars mt-1">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+            </div>
+        </div>
+    `;
+}
 
 axios.get('apiRequest/getComentarios.php', {
     params: {
-        id: 2
+        id
     }
 })
     .then(res => {
         console.log(res.data);
+        const plantilla = res.data.map(renderComentarios).join('');
+        // console.log(plantilla);
+        cajaComentarios.innerHTML = plantilla;
     })
     .catch(err => {
         console.log(err)
